f4a645c2f97e189fbd60c61b15de1e09bdc7ba2b
Yaroslav Emelyanov <canabis32rus@gmail.com>
2020-01-13 23:35:31 +0300

    Подготовка проекта

---
+++ src/layouts/CommonLayout.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="common">
+        <h1>Common</h1>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Common"
+    }
+</script>
+
+<style lang="scss" scoped>
+    .common {
+        background-color: blue;
+    }
+</style>
---
+++ src/layouts/EmptyLayout.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="empty">
+        <h1>Empty</h1>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Empty"
+    }
+</script>
+
+<style lang="scss" scoped>
+   .empty {
+       background-color: red;
+   }
+</style>
